38543d15f8a0339dafb45e51df851aab0fada50b
gnerkus <ifeanyioraelosi@gmail.com>
2021-09-04 15:11:35 +0200

    Create subdirectory for include files

--- include/example.hpp
+++ include/example.hpp
@@ -7,11 +7,11 @@ namespace example {
 
   //add two integers
   // this is the function to be exported
-  int add(int x, int y);
+  double add(double x, double y);
   
   //add function wrapper
   // this wrapper will call the function above
-  Napi::Number addWrapped(const Napi::CallbackInfo& info);
+  Napi::Value addWrapped(const Napi::CallbackInfo& info);
   
   //Export API
   Napi::Object Init(Napi::Env env, Napi::Object exports);
--- src/example.cpp
+++ src/example.cpp
@@ -2,36 +2,38 @@
 
 using namespace std;
 
-int example::add(int x, int y) {
+double example::add(double x, double y) {
   return x + y;
 }
 
 // TEMP comment
-// Napi::Number means Number is an object within the Napi namespace
 // :: is for namespaces
-Napi::Number example::addWrapped(const Napi::CallbackInfo& info) {
+Napi::Value example::addWrapped(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
-  
-  //check if arguments are integer only.
-  if(info.Length() < 2 || !info[0].IsNumber() || !info[1].IsNumber()){
-    Napi::TypeError::New(env, "arg1::Number, arg2::Number expected").ThrowAsJavaScriptException();
+
+  if(info.Length() < 2) {
+    Napi::TypeError::New(env, "Wrong number of arguments")
+        .ThrowAsJavaScriptException();
+    return env.Null();
   }
-  
-  //convert javascripts datatype to c++
-  // pull in function arguments
-  Napi::Number first = info[0].As<Napi::Number>();
-  Napi::Number second = info[1].As<Napi::Number>();
-  
-  //run c++ function return value and return it in javascript
+
+  if (!info[0].IsNumber() || !info[1].IsNumber()) {
+    Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  //  convert javascripts datatype to c++
+  double arg0 = info[0].As<Napi::Number>().DoubleValue();
+  double arg1 = info[1].As<Napi::Number>().DoubleValue();
   // Napi::Number::New(napi_env, double) converts the double to a Napi::Number
-  Napi::Number returnValue = Napi::Number::New(env, example::add(first.Int32Value(), second.Int32Value()));
- 
- return returnValue;
+  Napi::Number num = Napi::Number::New(env, example::add(arg0, arg1));
+
+  return num;
 }
 
 Napi::Object example::Init(Napi::Env env, Napi::Object exports) 
 {
   //export Napi function
-  exports.Set("add", Napi::Function::New(env, example::addWrapped));
+  exports.Set(Napi::String::New(env, "add"), Napi::Function::New(env, example::addWrapped));
   return exports;
 }
